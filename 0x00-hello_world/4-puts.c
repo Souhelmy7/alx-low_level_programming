218d5be388fbad4371f72884178970483beccc43
souhelmy7 <esraahelmy77@gmail.com>
2023-04-20 13:45:04 -0700

    ok commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,3 +3,10 @@
   *main - Entry point
   *puts - print a message
   *Return: Always 0 (Success)
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+
