c46d5adece0e6ccc20cd76d39851fc0b6dc6ff87
souhelmy7 <esraahelmy77@gmail.com>
2023-04-25 17:11:15 -0700

    ok commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
-{char letter;
+{
+char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
-        }
+}
 
 _putchar('\n');
 }
