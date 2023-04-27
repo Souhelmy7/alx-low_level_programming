bd05df7d328281faa2ef44f738d7a2f787ab7483
souhelmy7 <esraahelmy77@gmail.com>
2023-04-27 09:16:40 -0700

    ok commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*
+*-isuper - chech if c is upper
+*
+*0c : input for alphabet
+*
+* return: 1 if its uppercase. 0if not
+*/
+int -isuper(int c)
+{
+
+if (c >= 65 && c <= 90)
+
+return (1);
+else
+
+return (0);
+}
