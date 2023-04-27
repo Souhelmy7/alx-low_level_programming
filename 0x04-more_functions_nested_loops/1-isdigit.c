fa311d63e65784bfcd8924bd45706c7ebb0061c5
souhelmy7 <esraahelmy77@gmail.com>
2023-04-27 14:38:12 -0700

    ok commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isdigit - function that checks for uppercase character.
+*
+*@c: the character in ASCi table
+*
+* Return: 1 for uppercase. 0 for otherwise
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
+
