3381f1852c57fdfd8091da66030445226b6f28b4
souhelmy7 <esraahelmy77@gmail.com>
2023-04-27 14:40:17 -0700

    ok commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* mul - function that checks for uppercase character.
+*
+*@a: the first number
+*
+*@b: the second number
+* Return: 1 for uppercase. 0 for otherwise
+*/
+
+int mul(int a, int b)
+{
+int mul;
+
+mul = a * b;
+return (mul);
+}
