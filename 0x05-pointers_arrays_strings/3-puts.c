b767940952540226efa527c4a6392eb2788e2a87
souhelmy7 <esraahelmy77@gmail.com>
2023-05-02 14:36:31 -0700

    ok commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
-* _puts - prints a string
-* @str: string to print
-*
-* description: prints astring
-* on succces: return the number of characters printed
+*  _puts - function that prints a string
+* @str: the pointer that holds the value of pointer str
 */
-
 void _puts(char *str)
 {
-	while (*str)
-	{
-		_putchar(*str++);
-	
-	_putchar('\n');
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
 }
+_putchar('\n');
+}
+
